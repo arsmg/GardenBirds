8b3cf483a3d547b23e4bad35398c6103443c0185
Anton Stamenov <antstamenov@gmail.com>
2018-01-14 16:56:22 +0200

    views initiazed in onCreate

--- app/src/main/java/com/example/android/gardenbirdsquiz/ByPicture.java
+++ app/src/main/java/com/example/android/gardenbirdsquiz/ByPicture.java
@@ -8,11 +8,22 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class ByPicture extends AppCompatActivity {
+
+    private RadioGroup radioQuestion1;
+    private RadioGroup radioQuestion2;
+    private RadioGroup radioQuestion3;
+    private CheckBox checkBox1;
+    private CheckBox checkBox2;
+    private CheckBox checkBox3;
+    private CheckBox checkBox4;
+    private EditText editText1;
+    private Button submitButton;
 
     int basescore = 0;
     int count = 0;
@@ -21,35 +32,69 @@ public class ByPicture extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_by_picture);
+
+        radioQuestion1 = (RadioGroup) findViewById(R.id.firstQuestion);
+        radioQuestion2 = (RadioGroup) findViewById(R.id.secondQuestion);
+        radioQuestion3 = (RadioGroup) findViewById(R.id.thirdQuestion);
+        checkBox1 = (CheckBox) findViewById(R.id.question4answer1);
+        checkBox2 = (CheckBox) findViewById(R.id.question4answer2);
+        checkBox3 = (CheckBox) findViewById(R.id.question4answer3);
+        checkBox4 = (CheckBox) findViewById(R.id.question4answer4);
+        editText1 = (EditText) findViewById(R.id.question5answer);
+        submitButton = (Button) findViewById(R.id.submit);
+
     }
 
 
     public void submitAnswer (View view) {
-        RadioButton radioButton1 = findViewById(R.id.radiobutton1);
-        boolean isRadioButton1Checked = radioButton1.isChecked();
 
-        RadioButton radioButton2 = findViewById(R.id.radiobutton2);
-        boolean isRadioButton2Checked = radioButton2.isChecked();
+        // I am checking if the right answer to Question 1 was given
+        int question1_id = radioQuestion1.getCheckedRadioButtonId();
+        RadioButton radioButton1 = (RadioButton) radioQuestion1.findViewById(question1_id);
+        String answer1_text = (String) radioButton1.getText();
+        if (answer1_text.equalsIgnoreCase(getString(R.string.Goldfinch))) {
+            basescore ++;
+        }
 
-        RadioButton radioButton3 = findViewById(R.id.radiobutton3);
-        boolean isRadioButton3Checked = radioButton3.isChecked();
+        // I am checking if the right answer to Question 2 was given
+        int question2_id = radioQuestion1.getCheckedRadioButtonId();
+        RadioButton radioButton2 = (RadioButton) radioQuestion2.findViewById(question2_id);
+        String answer2_text = (String) radioButton1.getText();
+        if (answer1_text.equalsIgnoreCase(getString(R.string.Bluetit))) {
+            basescore ++;
+        }
 
-        RadioButton radioButton4 = findViewById(R.id.radiobutton4);
-        boolean isRadioButton4Checked = radioButton4.isChecked();
+        // I am checking if the right answer to Question 3 was given
+        int question3_id = radioQuestion1.getCheckedRadioButtonId();
+        RadioButton radioButton3 = (RadioButton) radioQuestion3.findViewById(question3_id);
+        String answer3_text = (String) radioButton1.getText();
+        if (answer1_text.equalsIgnoreCase(getString(R.string.Bluetit))) {
+            basescore ++;
+        }
 
-        int score = calculateScore(isRadioButton1Checked, isRadioButton2Checked,
-                isRadioButton3Checked, isRadioButton4Checked);
+        // I am checking if the right answer to Question 4 was given
+        if (checkBox1.isChecked() && checkBox3.isChecked()){
+            basescore +=2;
+        }
+        else if(checkBox1.isChecked() || checkBox3.isChecked()) {
+            basescore ++;
+        }
 
-        String scoreMessage = scoreSummery(score);
+        // Toast message with the score
+         Toast.makeText(this, getString(R.string.result_summery) + basescore, Toast.LENGTH_SHORT).show();
 
-        if(count == 1) {
-            displayMessage(scoreMessage);}
-            else{
-            Toast.makeText(getApplicationContext(), "Wrong", Toast.LENGTH_SHORT).show();
-            }
+       if (submitButton.isClickable()){
+            count ++;
+        }
+
+        if(count > 1 && count < 1000){
+            Toast.makeText(this, getString(R.string.wrong_answer), Toast.LENGTH_SHORT).show();
+        }
+
         }
 
 
+    /*
     private int calculateScore(boolean radioButton1Checked, boolean radioButton2Checked,
                            boolean radioButton3Checked, boolean radioButton4Checked){
 
@@ -78,6 +123,6 @@ public class ByPicture extends AppCompatActivity {
     private void displayMessage(String scoreMessage) {
         TextView orderSummeryTextView = (TextView) findViewById(R.id.score);
         orderSummeryTextView.setText(scoreMessage);
-    }
+    } */
 
 }
